8293c001df9d32c80c35e433e317e20c54bf10ea
hzaemey <hzaemey@gmail.com>
2014-12-13 23:32:47 +1100

    initial push.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopButtonHide: UIButton!
+    
+    @IBAction func stopButtion(sender: UIButton) {
+        recordingInProgress.hidden = true
+        stopButtonHide.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func record(sender: UIButton) {
+        // Show text "Recording in progress..."
+        recordingInProgress.hidden = false
+        stopButtonHide.hidden = false
+        // Record voice from the user.
+        println("in recordAudio")
+        
+    }
 
 }
 
